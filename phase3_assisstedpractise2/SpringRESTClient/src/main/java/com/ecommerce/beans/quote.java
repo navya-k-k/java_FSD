07719f01962f99927bf16a861cc44c2340c1df36
navya-k-k <75126858+navya-k-k@users.noreply.github.com>
2023-06-10 13:06:26 +0530

    Add files via upload

---
+++ phase3_assisstedpractise2/SpringRESTClient/src/main/java/com/ecommerce/beans/quote.java
@@ -0,0 +1,38 @@
+package com.ecommerce.beans;
+import com.fasterxml.jackson.annotation.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class quote {
+
+    private String type;
+    private Value value;
+
+    public quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
